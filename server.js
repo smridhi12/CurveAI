251ed19970ffcd0af7e9fac9b41de1cbe4f86025
Jastagar <jastagar1581.be20@chitkara.edu.in>
2022-09-19 22:13:19 +0530

    initial Commit

---
+++ server.js
@@ -0,0 +1,48 @@
+require("dotenv").config()
+
+const express = require("express")
+const app = express();
+const multer = require("multer")
+const { spawn } = require('child_process')
+const path = require('path')
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'Images')
+    },
+    filename: (req, file, cb)=>{
+        cb(null, Date.now()+ path.extname(file.originalname))
+    }
+})
+
+
+const upload = multer({storage})
+
+app.use(express.static("public"))
+
+app.set("view engine","ejs")
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+app.get('/exe',(req,res)=>{
+    let bool = '';
+    const pythonOne = spawn('python', ['./python.py'])
+    pythonOne.stdout.on('data',(data)=>{
+        bool = data.toString()
+    })
+    pythonOne.on('close',(data)=>{
+        res.send(bool)
+    })
+    console.log(bool)
+})
+
+app.post("/upload", upload.single("file"),async (req,res)=>{
+    res.redirect("/exe")
+})
+
+
+app.listen(3000)
